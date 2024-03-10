c1281513c31ae1a5575a211066c255833f6bfb38
jadonharsh109 <jadonharsh109@gmail.com>
2024-03-10 14:37:26 +0530

    Fixing Dockerfile

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-uvicorn main:app --host 0.0.0.0 --port 8000 &
+uvicorn main:app --workers 10 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 &
 
 celery -A celery_worker.app worker --loglevel=info &
 
