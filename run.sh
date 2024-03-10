7488bab350a12aa9ec7b0163b0a9bb67ecddd2f4
jadonharsh109 <jadonharsh109@gmail.com>
2024-03-10 14:55:32 +0530

    Edited run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-uvicorn main:app --workers 10 --bind 0.0.0.0:8000 &
+uvicorn main:app --workers 5 --host 0.0.0.0 --port 8000 &
 
 celery -A celery_worker.app worker --loglevel=info &
 
