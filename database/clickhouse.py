a55e2cf05d4d7176e291c78cad81c8437675f099
jadonharsh109 <jadonharsh109@gmail.com>
2024-03-10 15:15:58 +0530

    Fixing Clickhouse connection

--- database/clickhouse.py
+++ database/clickhouse.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 from datetime import datetime, timezone
 
 # client = Client('localhost')
-client = Client('resend-webhook-clickhouse.gj54va.easypanel.host', port=443)
+client = Client('resend-webhook-clickhouse.gj54va.easypanel.host', port=80, secure=True)
 # -----------------------------------
 def create_database():
     client.execute('CREATE DATABASE IF NOT EXISTS webhook')
