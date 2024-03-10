8eead0a5ce1fc315c04ab4888d9233b0c74cd0ea
jadonharsh109 <jadonharsh109@gmail.com>
2024-03-10 14:42:48 +0530

    Fixing Bash

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-set -e
 
 uvicorn main:app --workers 10 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 &
 
 celery -A celery_worker.app worker --loglevel=info &
 
-# Execute the Dockerfile's original CMD
-exec "$@"
+wait
