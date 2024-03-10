efb08dca882d8e852160ae7599c8145a719dec57
jadonharsh109 <jadonharsh109@gmail.com>
2024-03-10 14:51:04 +0530

    Fixing BashScript

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-uvicorn main:app --workers 10 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 &
+uvicorn main:app --workers 10 --bind 0.0.0.0:8000 &
 
 celery -A celery_worker.app worker --loglevel=info &
 
