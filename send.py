9b6146e5d1b8a7d1a12f35ef4bda69ddae5893d7
Biniyamseid <Biniyamseid3@gmail.com>
2024-03-12 06:06:36 +0300

    update

--- send.py
+++ send.py
@@ -7,7 +7,7 @@ resend.api_key = "re_8ngxrF3m_3yBQBRVrbQsGDVkcBUWbjdgy"
 r = resend.Emails.send({
   "from": "onboarding@resend.dev",
   "to": "info@n8n.shop",
-  "subject": "New Message",
+  "subject": "New Message 4",
   "html": "<p>Congrats on sending your <strong>first email hello</strong>!</p>"
 })
 
