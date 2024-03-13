fd7e7031853e0a2df149b1e42a59e837876b5a7b
Biniyamseid <Biniyamseid3@gmail.com>
2024-03-13 17:18:08 +0300

    update:code clean up

--- database/clickhouse.py
+++ database/clickhouse.py
@@ -25,9 +25,7 @@ def create_table():
     """
     try:
         logger.info("Creating table...")
-        # client.execute("DROP TABLE IF EXISTS webhook.payloads")
         client.execute('''
-                       
     CREATE TABLE IF NOT EXISTS webhook.payloads (
         id UUID,
         sender String,
@@ -35,7 +33,7 @@ def create_table():
         subject String,
         email_id String,
         event_type String,
-        created_at Int32,  -- Changed from DateTime('UTC') to Int32
+        created_at DateTime('UTC'),
         INDEX sender_idx (sender) TYPE bloom_filter() GRANULARITY 1,
         INDEX event_type_idx (event_type) TYPE bloom_filter() GRANULARITY 1
     ) ENGINE = MergeTree()
@@ -55,18 +53,55 @@ def insert_payload(payload):
     email_id = payload['data']['email_id']
     event_type = payload['type']
     created_at = datetime.strptime(payload['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=timezone.utc)
-    created_at_unix = int(created_at.timestamp())  # Convert to Unix timestamp
 
     try:
         client.execute(
             'INSERT INTO webhook.payloads (id, sender, recipient, subject, email_id, event_type, created_at) VALUES',
-            [(id, sender, recipient, subject, email_id, event_type, created_at_unix)]
+            [(id, sender, recipient, subject, email_id, event_type, created_at)]
         )
         logger.info("Hardcoded data inserted successfully.")
     except errors.Error as e:
         logger.info(f"Failed to insert  data: {e}")
         raise
     
+
+def insert_h_data():
+    """
+    this function inserts hardcoded data into the payloads table for testing purposes.
+    """
+    payload = {
+        "created_at": "2024-03-10T11:41:31.198Z",
+        "data": {
+            "created_at": "2024-03-10T11:41:30.456Z",
+            "email_id": "f3043bc9-f183-4435-a378-907562703ea9",
+            "from": "onboarding@resend.dev",
+            "subject": "data",
+            "to": [
+                "example@example.com"
+            ]
+        },
+        "type": "email.delivered"
+    }
+
+    id = str(uuid4())
+    sender = payload['data']['from']
+    recipient = payload['data']['to'][0]
+    subject = payload['data']['subject']
+    email_id = payload['data']['email_id']
+    event_type = payload['type']
+    created_at = datetime.strptime(payload['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=timezone.utc)
+
+    try:
+        client.execute(
+            'INSERT INTO webhook.payloads (id, sender, recipient, subject, email_id, event_type, created_at) VALUES',
+            [(id, sender, recipient, subject, email_id, event_type, created_at)]
+        )
+        logger.info("Hardcoded data inserted successfully.")
+    except errors.Error as e:
+        logger.info(f"Failed to insert  data: {e}")
+        raise
+
+
 
 def get_payloads():
     try:
@@ -78,6 +113,8 @@ def get_payloads():
         logger.info(f"Failed to get payloads: {e}")
         raise
 
+# _____________________2
+# ... (other imports and functions)
 
 def query_payloads(sender, recipient=None, status=None, start_date=None, end_date=None, pagination_start=0, pagination_end=15):
     query = "SELECT * FROM webhook.payloads WHERE sender = %(sender)s"
@@ -94,13 +131,13 @@ def query_payloads(sender, recipient=None, status=None, start_date=None, end_dat
     if start_date:
         # Assuming start_date is already a datetime object
         start_timestamp = int(start_date.timestamp())
-        query += " AND created_at >= %(start_timestamp)s"
+        query += " AND created_at >= toDateTime(%(start_timestamp)s)"
         params['start_timestamp'] = start_timestamp
 
     if end_date:
         # Assuming end_date is already a datetime object
         end_timestamp = int(end_date.timestamp())
-        query += " AND created_at <= %(end_timestamp)s"
+        query += " AND created_at <= toDateTime(%(end_timestamp)s)"
         params['end_timestamp'] = end_timestamp
 
     # Add ORDER BY and LIMIT with OFFSET for pagination
@@ -128,5 +165,3 @@ def get_all_payloads():
     except errors.Error as e:
         logger.info(f"Failed to get all payloads: {e}")
         raise
-
-
