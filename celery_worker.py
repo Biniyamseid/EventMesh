6a0b19b70ce375d4f1bcecfad8b52f25665658d2
Biniyamseid <Biniyamseid3@gmail.com>
2024-03-13 18:40:10 +0300

    feat: update the code to enable celery beat

--- celery_worker.py
+++ celery_worker.py
@@ -1,7 +1,10 @@
+from datetime import datetime, timedelta, timezone
+from datetime import datetime
 from celery import Celery
 from database.clickhouse import insert_payload, create_database, create_table,insert_h_data,insert_payload
 app = Celery('tasks', broker='redis://default:8c3e85e077fd42b5264c@resend_webhook_redis_server:6379/0',backend='db+sqlite:////app/results.db')
-
+from database.clickhouse import client
+from celery.schedules import crontab
 create_database()
 create_table()
 
@@ -16,3 +19,17 @@ def process_webhook_payload(self, payload):
         self.update_state(state='FAILURE', meta=str(e))
         raise
 
+
+@app.task
+def cleanup_old_records():
+    threshold = int((datetime.now(datetime.timezone.utc) - timedelta(days=60)).timestamp())
+    delete_query = "ALTER TABLE webhook.payloads DELETE WHERE created_at < %(threshold)s"
+    client.execute(delete_query, {'threshold': threshold})
+
+
+app.conf.beat_schedule = {
+    'cleanup-every-60-days': {
+        'task': 'resend_webhook.celery_worker.cleanup_old_records',
+        'schedule': timedelta(days=60),
+    },
+}
